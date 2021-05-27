eb812aeab1a95de6699e6d8bf109dccb9336e014
ali behroozi <ali.bhrzinia@gmail.com>
2021-05-27 14:33:24 +0430

    Open professors list fragment in main activity

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.R
+import com.example.myapplication.ui.professorsList.ProfessorListFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,5 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.flContainer , ProfessorListFragment.newInstance())
+        transaction.commit()
     }
 }

8fd00794a10b79fde31c808c593e722f726713bb
ali behroozi <ali.bhrzinia@gmail.com>
2021-05-27 11:52:59 +0430

    Remove redundent code from main activity

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -5,22 +5,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.myapplication.ProfessorListViewModel
 import com.example.myapplication.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val professorListViewModel: ProfessorListViewModel by lazy {
-        ViewModelProvider(this)[ProfessorListViewModel::class.java]
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        professorListViewModel.professors.observe(this, Observer {
-            Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
-        })
     }
 }
